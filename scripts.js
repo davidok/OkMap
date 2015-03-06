606803bf05e814ae471da92ea2c2d32b1a8bac0a
David Ok <davidok@gmail.com>
2015-03-06 02:19:29 -0500

    Corrects name of variable in JavaScript file referencing data file variable.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-var map = L.map('healthCenterMap').setView([40.714269,-74.005973], 13);
+var map = L.map('healthCenterMap').setView([40.714269, -74.005973], 13);
 
 // add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	attribution : '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var nycCenters = nycCentersData.rows;
+var nycCenters = healthCenterData.rows; 
