9f644544b7e830d5dd4a826d5a31af78d5ecc31e
David Ok <davidok@gmail.com>
2015-03-06 02:14:24 -0500

    Updated latitude and longitude in scripts file.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-var map = L.map('healthCenterMap').setView([51.505, -0.09], 13);
+var map = L.map('healthCenterMap').setView([40.714269,-74.005973], 13);
 
 // add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
