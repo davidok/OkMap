bcadb9405cf3407d61209dc75247e3515d9ac9e4
David Ok <davidok@gmail.com>
2015-03-06 02:05:10 -0500

    Added tile layer to scripts file.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
 var map = L.map('healthCenterMap').setView([51.505, -0.09], 13);
+
+// add an OpenStreetMap tile layer
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
