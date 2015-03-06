1b9f93ee7bdd9429c021abbb0daec8bfc3da77dd
David Ok <davidok@gmail.com>
2015-03-06 02:25:56 -0500

    Added for loop in JavaScript file to add markers for each point. Added reference to data in index file.

--- scripts.js
+++ scripts.js
@@ -5,4 +5,10 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 	attribution : '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var nycCenters = healthCenterData.rows; 
+var nycCenters = healthCenterData.rows;
+
+for (var i = 0; i < nycCenters.length; i++) {
+	var individualHealthCenter = nycCenters[i]
+	var marker = L.marker([individualHealthCenter.latitude, individualHealthCenter.longitude]).addTo(map);
+	marker.bindPopup("<b>" + individualHealthCenter.FullAddress + "</b><br>" + individualHealthCenter.ProgramName);
+} //end of for loop
